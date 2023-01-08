49fc1a14c7da7c289b11c6b7a47db21faa691436
Trevin <trevin_rambukpotha@hotmail.com>
2023-01-08 14:38:54 +1100

    Added Bubble Sort algorithm

---
+++ src/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+
+    /*
+    One of the least efficient algorithm, it's very slow.
+    Big O complexity: O(n^2)
+     */
+    public static void Sort(int[] array) {
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
+
+        System.out.println("\n-Sorted-");
+
+        BubbleSort.Sort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
     }
 }
