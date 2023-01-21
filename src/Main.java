0f3b4e2a5799ebf285a4401eb9370086952c374b
Drogshell <trevin_rambukpotha@hotmail.com>
2023-01-21 21:14:29 +1100

    -------------- Updates / Fixes ------------------------ - Updated RadixSort.java with a new method to handle string arrays

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        int arrayLength = 10;
-        int[] array = new int[arrayLength];
-        Random random = new Random();
-
-        for (int i = 0; i < arrayLength; i++) {
-            array[i] = random.nextInt(201) - 100;
-        }
+//        int arrayLength = 10;
+//        int[] array = new int[arrayLength];
+//        Random random = new Random();
+//
+//        for (int i = 0; i < arrayLength; i++) {
+//            array[i] = random.nextInt(201) - 100;
+//        }
 
         //int[] array = {20, 35, -15, 7, 55, 1, -22};
         //int[] countingSortArray = {2,5,9,8,2,8,7,10,4,3};
         //int[] radixArray = {4725, 4586, 1330, 8792, 1594, 5729};
+        String[] array = {"bcdef", "dbaqc", "abcde", "omadd", "bbbbb"};
+
 
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + ",");
@@ -21,7 +23,7 @@ public class Main {
 
         long startTime = System.nanoTime();
 
-        QuickSort.Sort(array, 0, arrayLength);
+        RadixSort.SortString(array, 26, 5);
 
         long endTime = System.nanoTime();
 
--- src/RadixSort.java
+++ src/RadixSort.java
@@ -5,6 +5,12 @@ public class RadixSort {
         }
     }
 
+    public static void SortString(String[] input, int radix, int width) {
+        for (int i = width - 1; i >= 0; i--) {
+            radixSingleSortString(input, i, radix);
+        }
+    }
+
     private static void radixSingleSort(int[] input, int position, int radix) {
         int numItems = input.length;
         int[] countArray = new int[radix];
@@ -30,7 +36,35 @@ public class RadixSort {
         }
     }
 
+    private static void radixSingleSortString(String[] input, int position, int radix) {
+        int numItems = input.length;
+        int[] countArray = new int[radix];
+
+        for (String value : input) {
+            countArray[getIndex(position, value)]++;
+        }
+
+        //Adjust the count array
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        String[] temp = new String[numItems];
+
+        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getIndex(position, input[tempIndex])]] = input[tempIndex];
+        }
+
+        //Can use array copy
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+            input[tempIndex] = temp[tempIndex];
+        }
+    }
+
     private static int getDigit(int position, int value, int radix) {
         return value / (int) Math.pow(radix, position) % radix;
+    }
+    private static int getIndex(int position, String value) {
+        return value.charAt(position) - 'a';
     }
 }
